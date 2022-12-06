f6d5500943b22728d8a17deca08b5bc1eae0532c
Amr Salem <salem.amr241@gmail.com>
2022-12-05 20:50:20 -0500

    final

--- app.js
+++ app.js
@@ -4,8 +4,7 @@ const express =require('express');
 const mongoose =require('mongoose');
 const cors =require('cors');
 const app =express();
-const port = process.env.Port || 5000;
-
+const port = app.listen(process.env.PORT || 5000)
 //middlewares
 app.use(cors());
 app.use(express.json());
