a8ebbb55079ae27cd7a152f6abb022a00c1375bc
Amr Salem <salem.amr241@gmail.com>
2022-12-05 21:13:48 -0500

    amr

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ const express =require('express');
 const mongoose =require('mongoose');
 const cors =require('cors');
 const app =express();
-const port = process.env.PORT || 5000
 //middlewares
 app.use(cors());
 app.use(express.json());
@@ -28,9 +27,9 @@ mongoose.connect(
 
 app.use('/api/posts', require("./routes/routes"));
 if(process.env.NODE_ENV==='production'){
-    app.use(express.static(__dirname+'/dis/'));
+    app.use(express.static(__dirname+'/dist/'));
     app.get('*',(req,res)=>{
         res.sendFile(__dirname+'/dist/index.html');
     })
 }
-app.listen(port,()=> console.log(`server running at http://localhost:${port}`, ))
+app.listen(process.env.PORT || 5000,()=> console.log(`server running at http://localhost:${port}`, ))
