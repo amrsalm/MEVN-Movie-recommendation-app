60bd792dcccab2a799a8232238f383f3cc3d6a8c
Amr Salem <salem.amr241@gmail.com>
2022-12-05 19:12:16 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,37 @@
+//import 
+require("dotenv").config();
+const express =require('express');
+const mongoose =require('mongoose');
+const cors =require('cors');
+const app =express();
+const port = process.env.Port || 5000;
+
+//middlewares
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended :true}));
+app.use(express.static("uploads"));
+
+//database connection
+mongoose.connect(
+    process.env.DB_URI,
+    { useNewUrlParser: true, useUnifiedTopology: true,
+        useFindAndModify:true,
+        useCreateIndex:true},
+    function (err, res) {
+        try {
+            console.log('Connected to Database!');
+        } catch (err) {
+            throw err;
+        }
+    })
+  
+
+app.use('/api/posts', require("./routes/routes"));
+if(process.env.NODE_ENV=='production'){
+    app.use(express.static(__dirname+'/dis/'));
+    app.get('*',(req,res)=>{
+        res.sendFile(__dirname+'/dist/index.html');
+    })
+}
+app.listen(port,()=> console.log(`server running at http://localhost:${port}`, ))
---
+++ controllers/api.js
@@ -0,0 +1,92 @@
+const Post =require("../models/posts");
+
+module.exports=class api{
+    //fetch all posts 
+    static async fetchAllPost(req,res){
+        try{
+            const posts = await Post.find();
+            res.status(200).json(posts);
+
+        }
+        catch(err){
+            res.status(404).json({message : err.message});
+        }
+    }
+    //fetch post by ID 
+    static async fetchPostByID(req,res){
+    const id = req.params.id;
+    try{
+        const post= await Post.findById(id);
+        res.status(200).json(post);
+
+    }
+    catch (err){
+        res.status(404).json({message: err.message});
+    }
+    }
+    //create post
+    static async createPost(req,res){
+        const post = req.body;
+        const imagename= req.file.filename;
+        post.image = imagename;
+        try{
+            await Post.create(post);
+            res.status(201).json({message:"Post Created Successfully!"})
+        }
+        catch (err){
+            res.status(400).json({message: err.message});
+        }
+    }
+    //update post
+    static async updatePost(req,res){
+        const id = req.params.id;
+        let new_image='';
+        if(req.file){
+            new_image = req.file.filename;
+            
+        try{
+            FileSystem.unlinkSync('./uploads'+req.body.old_image);
+        }
+        catch(err){
+            console.log(err);
+        }
+    }    
+        else{
+            new_image=req.body.old_image;
+        }
+        const newPost = req.body;
+        newPost.image = new_image;
+         try{
+            await Post.findByIdAndUpdate(id, newPost);
+            res.status(200).json({message: "Post Updated Successfully!"})
+         }
+         catch(err){
+            res.status(404).json({message: err.message});
+
+         }
+    }
+    //delete post
+    static async deletePost(req,res){
+        const id = req.params.id;
+        try{
+            const result = await Post.findByIdAndDelete(id);
+            if(result.image != ''){
+                try{
+                    FileSystem.unlinkSync('./uploads/'+result.image);
+
+                }catch(err){
+                    console.log(err);
+
+                }
+                
+
+            }
+            res.status(200).json({message: 'Post Deleted Successfully!'});
+
+        }
+    catch(err){
+        res.status(404).json({message: err.message});
+    }
+
+    }
+};
